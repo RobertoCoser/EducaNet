970c3c452a934cc78a2600079737ed0a1960e1f3
Roberto Vigo Coser <robertovigocoser@gmail.com>
2025-07-02 23:42:41 -0300

    Ajustes na API

--- frontend/src/components/SchoolForm.jsx
+++ frontend/src/components/SchoolForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import api from '../api'; // Instância do Axios
+import api from '../api/index'; // Instância do Axios
 
 const SchoolForm = ({ currentSchool, onClose, onSave }) => {
   const [formData, setFormData] = useState({
--- frontend/src/pages/Schools.jsx
+++ frontend/src/pages/Schools.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import api from '../api'; // Instância do Axios
+import api from '../api/index'; // Instância do Axios
 import SchoolForm from '../components/SchoolForm';
 
 const Schools = () => {
